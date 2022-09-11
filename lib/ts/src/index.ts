2734c1319496ab9b02a47b317af0114e721023df
Antoine Duchâteau <ad@taktik.com>
2022-09-11 08:20:50 +0200

    Firs work on docker compose setup lig in ts

---
+++ lib/ts/src/index.ts
@@ -0,0 +1,63 @@
+import * as https from 'https'
+import * as fs from 'fs'
+import * as path from 'path'
+import { exec } from 'child_process'
+import * as util from 'util'
+
+const standardEnv = {
+  COUCHDB_PORT: '15984',
+  AS_PORT: '16044',
+  COUCHDB_USER: 'icure',
+  COUCHDB_PASSWORD: 'icure',
+  ...process.env,
+}
+
+function fullUrl(composeFile: string) {
+  return composeFile.startsWith('https') ? composeFile : `https://raw.githubusercontent.com/icure-io/icure-e2e-test-setup/master/${composeFile}.yaml`
+}
+
+export const setup = async (scratchDir: string, compose: string, ...profiles: string[]) => {
+  const composeFile = await download(scratchDir, fullUrl(compose))
+  try {
+    const { stdout, stderr } = await util.promisify(exec)(`/usr/local/bin/docker compose -f '${composeFile}' ${profiles.map((p) => `--profile ${p}`).join(' ')} up -d`, {
+      env: standardEnv,
+    })
+    console.log(`stdout: ${stdout}`)
+    console.error(`stderr: ${stderr}`)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+export const cleanup = async (scratchDir: string, compose: string) => {
+  try {
+    const composeFile = await download(scratchDir, fullUrl(compose))
+    const { stdout, stderr } = await util.promisify(exec)(`/usr/local/bin/docker compose -f '${composeFile}' down`, { env: standardEnv })
+    console.log(`stdout: ${stdout}`)
+    console.error(`stderr: ${stderr}`)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+async function download(scratchDir: string, url: string) {
+  let fullPath = path.join(scratchDir, path.basename(url))
+  const file = fs.createWriteStream(fullPath)
+  return new Promise<string>((resolve, reject) => {
+    https.get(url, function (response) {
+      response.pipe(file)
+
+      // after download completed close filestream
+      file.on('finish', () => {
+        file.close()
+        console.log('Download Completed')
+        resolve(fullPath)
+      })
+
+      file.on('error', (err) => {
+        console.log('Download Failed')
+        reject(err)
+      })
+    })
+  })
+}
---
+++ lib/ts/test/test-setup.ts
@@ -0,0 +1,51 @@
+import { afterEach, describe } from 'mocha'
+import { cleanup, setup } from '../src'
+import axios from 'axios'
+import { expect } from 'chai'
+
+const retry = (fn: () => Promise<any>, retriesLeft = 5, interval = 1000): Promise<any> => {
+  return fn().catch((err) => {
+    console.log('Retrying in ' + interval + 'ms', err)
+    return new Promise((resolve) => setTimeout(() => resolve(retry(fn, retriesLeft - 1, interval * 2)), interval)).then(() => fn())
+  })
+}
+
+async function checkCouchDbStarted() {
+  await retry(async () => {
+    const {
+      data: { couchdb },
+      status,
+    } = await axios.get('http://127.0.0.1:5984')
+    expect(status).to.equal(200)
+    expect(couchdb).to.equal('Welcome')
+  })
+}
+
+async function checkMsgGwStarted() {
+  await retry(async () => {
+    const {
+      data: { pong },
+      status,
+    } = await axios.get('http://127.0.0.1:8080/ping')
+    expect(status).to.equal(200)
+    expect(pong).to.match(/1\d+/)
+  })
+}
+
+describe('Test setup', () => {
+  afterEach(() => cleanup('test', 'docker-compose'))
+
+  it('should start the docker with long urls', async () => {
+    await setup('test', 'https://raw.githubusercontent.com/icure-io/icure-e2e-test-setup/master/docker-compose.yaml')
+    await checkCouchDbStarted()
+  })
+  it('should start the docker with short urls', async () => {
+    await setup('test', 'docker-compose')
+    await checkCouchDbStarted()
+  })
+  it('profiles let you control the launched containers', async () => {
+    await setup('test', 'docker-compose', 'msg-gw')
+    await checkCouchDbStarted()
+    await checkMsgGwStarted()
+  })
+})
