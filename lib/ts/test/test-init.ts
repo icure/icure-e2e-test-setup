da6d1d525c2e6f91aea2f21560ce72ef98563cde
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-09-23 10:32:46 +0200

    MDT-152 #cooment added check function and test

--- lib/ts/test/test-init.ts
+++ lib/ts/test/test-init.ts
@@ -1,31 +1,49 @@
-import { bootstrapOssKraken, setup, bootstrapCloudKraken, cleanup, setupCouchDb } from '../src';
-import { checkCouchDbStarted, checkExistence } from './utils';
-import uuid = require('uuid');
+import { bootstrapOssKraken, setup, bootstrapCloudKraken, cleanup, setupCouchDb, checkIfDockerIsOnline } from '../src'
+import { checkCouchDbStarted, checkExistence } from './utils'
+import uuid = require('uuid')
+import { expect } from 'chai'
 
-describe("Test initialization", () => {
+describe('Test initialization', () => {
+  it('Should be able to initialize the OSS Kraken', async () => {
+    await setup('test/scratch', process.env.OSS_DOCKER_URL!)
+    await checkCouchDbStarted()
+    await setupCouchDb('127.0.0.1', 15984)
+    const userId = uuid()
+    await bootstrapOssKraken(userId)
+    await checkExistence('127.0.0.1', 15984, 'icure-base', userId)
+    await cleanup('test/scratch', process.env.OSS_DOCKER_URL!)
+  })
 
-  it("Should be able to initialize the OSS Kraken", async () => {
-    await setup('test/scratch', process.env.OSS_DOCKER_URL!);
-    await checkCouchDbStarted();
-    await setupCouchDb('127.0.0.1', 15984);
-    const userId = uuid();
-    await bootstrapOssKraken(userId);
-    await checkExistence('127.0.0.1', 15984, 'icure-base', userId);
-    await cleanup('test/scratch', process.env.OSS_DOCKER_URL!);
-  });
+  it('Should be able to initialize the Kraken', async () => {
+    await setup('test/scratch', process.env.KRAKEN_DOCKER_URL!)
+    await checkCouchDbStarted()
+    await setupCouchDb('127.0.0.1', 15984)
+    const userId = uuid()
+    const groupId = 'xx'
+    await bootstrapCloudKraken(userId)
+    await checkExistence('127.0.0.1', 15984, 'icure-xx-base', userId)
+    await checkExistence('127.0.0.1', 15984, 'icure-__-base', `${groupId}:${userId}`)
+    await checkExistence('127.0.0.1', 15984, '_users', `org.couchdb.user:${groupId}`)
+    await checkExistence('127.0.0.1', 15984, 'icure-__-config', groupId)
+    await cleanup('test/scratch', process.env.KRAKEN_DOCKER_URL!)
+  })
 
-  it("Should be able to initialize the Kraken", async () => {
-    await setup('test/scratch', process.env.KRAKEN_DOCKER_URL!);
-    await checkCouchDbStarted();
-    await setupCouchDb('127.0.0.1', 15984);
-    const userId = uuid();
-    const groupId = "xx"
-    await bootstrapCloudKraken(userId);
-    await checkExistence('127.0.0.1', 15984, 'icure-xx-base', userId);
-    await checkExistence('127.0.0.1', 15984, 'icure-__-base', `${groupId}:${userId}`);
-    await checkExistence('127.0.0.1', 15984, '_users', `org.couchdb.user:${groupId}`);
-    await checkExistence('127.0.0.1', 15984, 'icure-__-config', groupId);
-    await cleanup('test/scratch', process.env.KRAKEN_DOCKER_URL!);
-  });
+  it('Should be able to check if a docker compose is running', async () => {
+    await setup('test/scratch', process.env.OSS_DOCKER_URL!)
+    await checkCouchDbStarted()
+    await setupCouchDb('127.0.0.1', 15984)
+    const userId = uuid()
+    await bootstrapOssKraken(userId)
+    const isUp = await checkIfDockerIsOnline('test/scratch', process.env.OSS_DOCKER_URL!)
+    expect(isUp).to.eq(true)
+  })
 
-});
+  it('Should be able to check if a docker compose is not running', async () => {
+    await setup('test/scratch', process.env.OSS_DOCKER_URL!)
+    await checkCouchDbStarted()
+    await setupCouchDb('127.0.0.1', 15984)
+    await cleanup('test/scratch', process.env.OSS_DOCKER_URL!)
+    const isUp = await checkIfDockerIsOnline('test/scratch', process.env.OSS_DOCKER_URL!)
+    expect(isUp).to.eq(false)
+  })
+})
