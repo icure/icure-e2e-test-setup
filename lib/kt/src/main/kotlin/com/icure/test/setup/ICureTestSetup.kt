08b8f695f7d9f6665da77bca697acde9fc3be9ad
Lisa Trapletti <lt@taktik.com>
2022-12-01 14:14:00 +0100

    Revert "Add Bootstrap OSS and cloud functions in Kotlin SDK"
    This reverts commit d0d6e722

--- lib/kt/src/main/kotlin/com/icure/test/setup/ICureTestSetup.kt
+++ lib/kt/src/main/kotlin/com/icure/test/setup/ICureTestSetup.kt
@@ -1,21 +1,45 @@
 package com.icure.test.setup
 
-import com.icure.test.setup.CouchDbRestClient.configureCouchDbClusterSetup
-import com.icure.test.setup.CouchDbRestClient.createNewDatabaseForGroup
-import com.icure.test.setup.CouchDbRestClient.createNewDbUser
-import com.icure.test.setup.CouchDbRestClient.createNewGroupInConfig
-import com.icure.test.setup.CouchDbRestClient.createNewUserIn
-import com.icure.test.setup.CouchDbRestClient.createNewUserInBase
-import com.icure.test.setup.CouchDbRestClient.isClusterSetup
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.basicAuth
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.URLBuilder
+import io.ktor.http.contentType
+import io.ktor.http.isSuccess
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.utils.io.errors.IOException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.retry
 import java.util.UUID
 import java.util.concurrent.TimeUnit
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 object ICureTestSetup {
-    suspend fun startCouchDbContainer(localPort: String = "15984", couchDbUser: String = "icure", couchDbPassword: String = "icure"): String {
+    private val httpClient = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+
+
+    suspend fun startCouchDbContainer(couchDbUser: String = "icure", couchDbPassword: String = "icure"): String {
         val dockerId = UUID.randomUUID().toString().substring(0, 6)
         val started = execute("/usr/local/bin/docker run " +
-                "-p $localPort:5984 " +
+                "-p 15984:5984 " +
                 "-e COUCHDB_USER=$couchDbUser -e COUCHDB_PASSWORD=$couchDbPassword -e ERL_MAX_PORT=16384 " +
                 "-d --name couchdb-test-${dockerId} " +
                 "couchdb:3.2.2")
@@ -24,9 +48,10 @@ object ICureTestSetup {
             throw RuntimeException("Could not start CouchDB Docker Container within 2 minutes")
         }
 
-        configureCouchDbClusterSetup("http://127.0.0.1:$localPort", couchDbUser, couchDbPassword).collect()
+        configCouchDb(couchDbUser, couchDbPassword)
+            .collect()
 
-        isClusterSetup("http://127.0.0.1:$localPort", couchDbUser, couchDbPassword).collect {
+        isClusterSetup(couchDbUser, couchDbPassword).collect {
             if (!it) {
                 throw RuntimeException("Could not finalize CouchDB Cluster Setup")
             }
@@ -45,31 +70,52 @@ object ICureTestSetup {
             false
     }
 
-    suspend fun bootstrapOss(userId: String = UUID.randomUUID().toString(),
-                             userLogin: String = "john",
-                             userPasswordHash: String = "1796980233375ccd113c972d946b2c4a7892e4f69c60684cfa730150047f9c0b",
-                             couchDbUrl: String = "http://127.0.0.1:15984",
-                             couchDbUser: String = "icure",
-                             couchDbPassword: String = "icure") {
-        createNewUserInBase(null, userId, userLogin, userPasswordHash, couchDbUrl, couchDbUser, couchDbPassword)
-    }
-
-    suspend fun bootstrapCloud(groupId: String = "xx",
-                               groupPassword: String = "xx",
-                               groupUserId: String = UUID.randomUUID().toString(),
-                               groupUserLogin: String = "john",
-                               groupUserPasswordHash: String = "1796980233375ccd113c972d946b2c4a7892e4f69c60684cfa730150047f9c0b",
-                               couchDbUrl: String = "http://127.0.0.1:15984",
-                               couchDbUser: String = "icure",
-                               couchDbPassword: String = "icure") {
-        createNewDatabaseForGroup(groupId, couchDbUrl, couchDbUser, couchDbPassword)
-        createNewUserIn(groupId, groupUserId, groupUserLogin, groupUserPasswordHash, couchDbUrl, couchDbUser, couchDbPassword)
-        createNewUserInBase(groupId, groupUserId, groupUserLogin, groupUserPasswordHash, couchDbUrl, couchDbUser, couchDbPassword)
-        createNewDbUser(groupId, groupPassword, couchDbUrl, couchDbUser, couchDbPassword)
-        createNewGroupInConfig(groupId, groupPassword, couchDbUrl, couchDbUser, couchDbPassword)
-    }
-
     private fun execute(command: String): Boolean = ProcessBuilder(command.split(' '))
         .start()
         .waitFor(2, TimeUnit.MINUTES)
+
+    private suspend fun configCouchDb(couchDbUser: String = "icure", couchDbPassword: String = "icure"): Flow<Unit> = flow<Unit> {
+        httpClient.post("http://127.0.0.1:15984/_cluster_setup") {
+            basicAuth(couchDbUser, couchDbPassword)
+            contentType(ContentType.Application.Json)
+            setBody("{\n" +
+                    "  \"action\" : \"enable_single_node\",\n" +
+                    "  \"username\" : \"$couchDbUser\",\n" +
+                    "  \"password\" : \"$couchDbPassword\",\n" +
+                    "  \"bind_address\" : \"0.0.0.0\",\n" +
+                    "  \"port\" : 5984,\n" +
+                    "  \"singlenode\" : true\n" +
+                    "}")
+        }
+    }.retry(5) {
+        delay(2.seconds)
+        println("Could not execute operation: ${it.message}")
+        true
+    }
+
+    private suspend fun isClusterSetup(couchDbUser: String = "icure", couchDbPassword: String = "icure") = flow<ClusterSetupStatus> {
+        val response = httpClient.get("http://127.0.0.1:15984/_cluster_setup") {
+            basicAuth(couchDbUser, couchDbPassword)
+            contentType(ContentType.Application.Json)
+        }
+
+        if (response.status.isSuccess()) {
+            val status = response.body<ClusterSetupStatus>()
+            if (status.state != "cluster_finished" && status.state != "single_node_enabled") {
+                throw RuntimeException("Cluster not setup yet: ${status.state}")
+            }
+        } else {
+            throw RuntimeException("Cluster not setup: Error ${response.status} - ${response.bodyAsText()}")
+        }
+
+    }.retry(5) {
+        delay(5.seconds)
+        println("Error during ClusterSetup Status check: ${it.message}")
+        true
+    }.map {
+        it.state != "cluster_finished" && it.state != "single_node_enabled"
+    }
+
+    @kotlinx.serialization.Serializable
+    data class ClusterSetupStatus(val state: String)
 }
--- lib/kt/src/test/kotlin/com/icure/test/setup/TestSetupTest.kt
+++ lib/kt/src/test/kotlin/com/icure/test/setup/TestSetupTest.kt
@@ -3,26 +3,8 @@ package com.icure.test.setup
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
-import io.kotest.matchers.string.shouldNotContain
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.basicAuth
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
-import io.ktor.http.isSuccess
-import io.ktor.serialization.kotlinx.json.json
-import java.util.UUID
 
 internal class TestSetupTest: StringSpec({
-    val httpClient = HttpClient(CIO) {
-        install(ContentNegotiation) {
-            json()
-        }
-    }
-
     "Start Docker CouchDB Successfully" {
         val dockerContainer = ICureTestSetup.startCouchDbContainer()
         dockerContainer shouldContain "couchdb-test-"
@@ -32,101 +14,10 @@ internal class TestSetupTest: StringSpec({
     }
 
     "Start Docker CouchDB Successfully With other credentials" {
-        val dockerContainer = ICureTestSetup.startCouchDbContainer("15985", "test", "test")
+        val dockerContainer = ICureTestSetup.startCouchDbContainer("test", "test")
         dockerContainer shouldContain "couchdb-test-"
 
         val cleaned = ICureTestSetup.cleanContainer(dockerContainer)
         cleaned shouldBe true
     }
-
-    "Init a Kraken environment Successfully" {
-        val dockerContainer = ICureTestSetup.startCouchDbContainer()
-        CouchDbRestClient.createNewDatabase("icure-__-base", couchDbUser = "icure", couchDbPassword = "icure")
-        CouchDbRestClient.createNewDatabase("icure-__-config", couchDbUser = "icure", couchDbPassword = "icure")
-
-        val userId = UUID.randomUUID().toString()
-
-        // When
-        ICureTestSetup.bootstrapCloud("xx", "xx", userId, "john")
-
-        // Then
-        checkDatabaseExists(httpClient, "xx", "icure", "icure")
-        checkUserExistsInGroup(httpClient, "xx", userId, "john", "icure", "icure")
-        checkUserExistsInBase(httpClient, "xx:$userId", "john", "icure", "icure")
-
-        // Finally
-        ICureTestSetup.cleanContainer(dockerContainer)
-    }
-
-    "Init a OSS environment Successfully" {
-        val dockerContainer = ICureTestSetup.startCouchDbContainer()
-        CouchDbRestClient.createNewDatabase("icure-base", couchDbUser = "icure", couchDbPassword = "icure")
-
-        val userId = UUID.randomUUID().toString()
-
-        // When
-        ICureTestSetup.bootstrapOss(userId, "john")
-
-        // Then
-        checkUserExistsInBase(httpClient, userId, "john", couchDbUser = "icure", couchDbPassword ="icure")
-
-        // Finally
-        ICureTestSetup.cleanContainer(dockerContainer)
-    }
 })
-
-private suspend fun checkDatabaseExists(httpClient: HttpClient, groupId: String, couchDbUser: String, couchDbPassword: String) {
-    val response = httpClient.get("http://127.0.0.1:15984/icure-$groupId-base") {
-        basicAuth(couchDbUser, couchDbPassword)
-        contentType(ContentType.Application.Json)
-    }
-
-    response.status.isSuccess() shouldBe true
-}
-
-private suspend fun checkUserExistsInGroup(httpClient: HttpClient,
-                                    groupId: String,
-                                    userId: String,
-                                    userLogin: String,
-                                    couchDbUser: String,
-                                    couchDbPassword: String) {
-    val response = httpClient.get("http://127.0.0.1:15984/icure-$groupId-base/$userId") {
-        basicAuth(couchDbUser, couchDbPassword)
-        contentType(ContentType.Application.Json)
-    }
-    response.status.isSuccess() shouldBe true
-
-    val responseBody = response.bodyAsText()
-    responseBody shouldContain userId
-    responseBody shouldContain userLogin
-    responseBody shouldContain "\"isUse2fa\":true"
-    responseBody shouldContain "\"type\":\"database\""
-    responseBody shouldContain "\"status\":\"ACTIVE\""
-    responseBody shouldContain "\"java_type\":\"org.taktik.icure.entities.User\""
-}
-
-private suspend fun checkUserExistsInBase(httpClient: HttpClient,
-                                           userId: String,
-                                           userLogin: String,
-                                           couchDbUser: String,
-                                           couchDbPassword: String) {
-    val baseDatabaseName = if (userId.contains(':')) "icure-__-base" else "icure-base"
-    val response = httpClient.get("http://127.0.0.1:15984/$baseDatabaseName/$userId") {
-        basicAuth(couchDbUser, couchDbPassword)
-        contentType(ContentType.Application.Json)
-    }
-    response.status.isSuccess() shouldBe true
-
-    val responseBody = response.bodyAsText()
-    responseBody shouldContain userId
-    responseBody shouldContain userLogin
-    responseBody shouldContain "\"type\":\"database\""
-    responseBody shouldContain "\"status\":\"ACTIVE\""
-    responseBody shouldContain "\"java_type\":\"org.taktik.icure.entities.User\""
-
-    if (userId.contains(':')) { // We are in the Cloud context
-        responseBody shouldContain "org.taktik.icure.entities.security.AlwaysPermissionItem"
-    } else {
-        responseBody shouldNotContain "org.taktik.icure.entities.security.AlwaysPermissionItem"
-    }
-}
